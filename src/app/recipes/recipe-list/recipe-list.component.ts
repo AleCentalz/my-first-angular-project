3f6140c402e095034c4e41ec5ddb9e46f2cfc3f9
AleCentalz <palkya58@gmail.com>
2022-05-25 12:34:56 -0500

    initial commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('Pasta', 'This is a simply test', 'https://www.manutd-malta.com/wp-content/uploads/2022/03/pasta.jpg'),
+    new Recipe('Cake', 'This is a simply test', 'https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fimg1.cookinglight.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2F4_3_horizontal_-_1200x900%2Fpublic%2F1542062283%2Fchocolate-and-cream-layer-cake-1812-cover.jpg%3Fitok%3DR_xDiShk')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
